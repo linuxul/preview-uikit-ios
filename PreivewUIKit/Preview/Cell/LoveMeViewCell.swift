0ce8615781fd83a41863ef43f779b77ba8ec759e
linuxul <jsseo@eflash.co.kr>
2023-08-14 22:57:00 +0900

    UIView & Xib - Preview 테스트 코드

--- PreivewUIKit/Preview/Cell/LoveMeViewCell.swift
+++ PreivewUIKit/Preview/Cell/LoveMeViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoveMeViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,5 +23,37 @@ class LoveMeViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func setData(_ title: String) {
+        titleLabel.text = title
+    }
+    
 }
 
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct LoveMeViewCellRepresentable: UIViewRepresentable {
+    typealias UIViewType = LoveMeViewCell
+
+    func makeUIView(context: Context) -> LoveMeViewCell {
+        return UINib(nibName: "LoveMeViewCell", bundle: nil)
+                            .instantiate(withOwner: nil, options: nil).first as! LoveMeViewCell
+    }
+
+    func updateUIView(_ uiView: LoveMeViewCell, context: Context) {
+        uiView.setData("Touch Me!!")
+    }
+}
+
+@available(iOS 13.0, *)
+struct LoveMeViewCellPreview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LoveMeViewCellRepresentable()
+                .frame(width: 375, height: 60)
+        }
+        .previewLayout(.sizeThatFits)
+        .padding(10)
+    }
+}
+#endif
