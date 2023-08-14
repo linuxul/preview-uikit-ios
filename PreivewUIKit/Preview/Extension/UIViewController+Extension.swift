dcc07a9f8a6e0f78797c99858eee825b54861dca
linuxul <jsseo@eflash.co.kr>
2023-08-14 23:01:19 +0900

    주석 생성

--- PreivewUIKit/Preview/Extension/UIViewController+Extension.swift
+++ PreivewUIKit/Preview/Extension/UIViewController+Extension.swift
@@ -14,14 +14,23 @@ import SwiftUI
 extension UIViewController {
     private struct Preview: UIViewControllerRepresentable {
         let viewController: UIViewController
-
+        
+        /// SwiftUI에서 UIViewController를 생성합니다.
+        /// - Parameter context: 현재 컨텍스트
+        /// - Returns: 생성된 UIViewController
         func makeUIViewController(context: Context) -> UIViewController {
             return viewController
         }
-
+        
+        /// UIViewController를 업데이트합니다. (이 경우 구현이 필요 없습니다.)
+        /// - Parameters:
+        ///   - uiViewController: 업데이트할 UIViewController
+        ///   - context: 현재 컨텍스트
         func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
     }
-
+    
+    /// 현재 UIViewController 인스턴스를 SwiftUI 뷰로 변환합니다.
+    /// - Returns: UIViewController를 나타내는 SwiftUI 뷰
     func toPreview() -> some View {
         Preview(viewController: self)
     }
@@ -31,14 +40,23 @@ extension UIViewController {
 public struct UIViewPreview<View: UIView>: UIViewRepresentable {
     public let view: View
     
+    /// UIView를 생성하는 초기화 메서드
+    /// - Parameter builder: UIView를 생성하는 클로저
     public init(_ builder: @escaping () -> View) {
         view = builder()
     }
     
+    /// SwiftUI에서 UIView를 생성합니다.
+    /// - Parameter context: 현재 컨텍스트
+    /// - Returns: 생성된 UIView
     public func makeUIView(context: Context) -> UIView {
         return view
     }
     
+    /// UIView를 업데이트합니다. 여기서는 hug priority를 설정합니다.
+    /// - Parameters:
+    ///   - view: 업데이트할 UIView
+    ///   - context: 현재 컨텍스트
     public func updateUIView(_ view: UIView, context: Context) {
         view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         view.setContentHuggingPriority(.defaultHigh, for: .vertical)
@@ -49,14 +67,23 @@ public struct UIViewPreview<View: UIView>: UIViewRepresentable {
 public struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
     public let viewController: ViewController
     
+    /// UIViewController를 생성하는 초기화 메서드
+    /// - Parameter builder: UIViewController를 생성하는 클로저
     public init(_ builder: @escaping () -> ViewController) {
         viewController = builder()
     }
     
+    /// SwiftUI에서 UIViewController를 생성합니다.
+    /// - Parameter context: 현재 컨텍스트
+    /// - Returns: 생성된 UIViewController
     public func makeUIViewController(context: Context) -> ViewController {
         return viewController
     }
     
+    /// UIViewController를 업데이트합니다. (이 경우 구현이 필요 없습니다.)
+    /// - Parameters:
+    ///   - uiViewController: 업데이트할 UIViewController
+    ///   - context: 현재 컨텍스트
     public func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {}
 }
 #endif
